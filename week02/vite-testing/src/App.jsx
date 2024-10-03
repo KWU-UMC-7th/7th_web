a95a2a791fd596e6a05e7ac79d2653307eed92ae
Riley_Looney <rileylooney01@gmail.com>
2024-10-03 13:36:12 +0900

    feat : 컴포넌트 분리

--- week02/vite-testing/src/App.jsx
+++ week02/vite-testing/src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const add_todo = () => {
+    alert("추가됨");
+  }
+
+  const drop_todo = () => {
+    alert("제거됨");
+  }
+
+  const update_todo = () => {
+    alert("수정됨");
+  }
 
   return (
     <>
-     <h1>Hello React</h1>
+      <div className='input_todo'>
+        <input type='text'></input>
+        <input type='button' value='할 일 등록' onClick={add_todo}></input>
+      </div>
+      <div className='list_todo'>
+        <input type='button' value='수정' onClick={update_todo}></input>
+        <input type='button' value='삭제' onClick={drop_todo}></input>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
