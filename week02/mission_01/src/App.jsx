436caa5f073159d5c7c6120b6677a6dd06e0179e
Riley_Looney <rileylooney01@gmail.com>
2024-10-05 03:08:12 +0900

    feat : todo 삭제 기능 추가

--- week02/mission_01/src/App.jsx
+++ week02/mission_01/src/App.jsx
@@ -3,37 +3,53 @@ import {useState} from 'react';
 
 function App() {
 
-  const [todo_list, setTodo] = useState([
+  //todo list 세팅
+  const [todoElement, setTodo] = useState([
     'mission_01 완성',
     'mission_02 완성',
   ]);
 
+  //todo text박스에서 입력받기 위한 useState
+  const [inputTodo, setInputTdo] = useState();
+
+  //todo list에 새로운 값 추가
   const add_todo = () => {
-
+    setTodo((prev) => [
+      ...prev,        //이전에 있던 todo 그대로 가져오기
+      inputTodo, 
+    ]);
   }
 
-  const drop_todo = () => {
-    alert("삭제됨")
+  //todo list에 값 삭제
+  const drop_todo = (index) => {
+    //alert(`${index}가 삭제됨`)
+    alert(`(${todoElement[index]}) 삭제됨`);
+    setTodo((prev) => 
+      prev.filter((_, i) => i !== index
+    )); // 해당 인덱스를 제외한 새 배열 생성
   }
 
-  const update_todo = () => {
-    alert("수정됨")
-  }
+  //todo list의 값 수정
+  const update_todo = (index) => {
+    alert(`(${todoElement[index]}) 수정됨`); // 수정할 항목의 이름
+    
+};
 
+  //보여지는 화면
   return (
     <>
       <h1>UMC Week02 - mission_01</h1>
       <div className = "Input_Area">
-        <input type="text"></input>
+        <input type="text" value={inputTodo}></input>
         <input type="button" value="할 일 등록" onClick={add_todo}></input>
       </div>
       <div>
         <ul>
-          {todo_list.map((todo, index) => (
+          {todoElement.map((todo, index) => (
             <div className='todoElement'>
-              <span key={index}> {todo}</span>
-              <input type="button" value="삭제하기" onClick={drop_todo}></input>
-              <input type="button" value="수정진행" onClick={update_todo}></input>
+              <span key={index}>{index} : {todo}</span>
+              <input type="button" value="삭제하기" onClick={() => drop_todo(index)}></input> 
+              <input type="button" value="수정진행" onClick={() => update_todo(index)}></input>
             </div>
           ))}
         </ul>
