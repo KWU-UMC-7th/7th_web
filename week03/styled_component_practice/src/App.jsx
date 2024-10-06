966d03085c30a252540a8336a8850976f8adb2d0
mmdh1234 <mmdh1234@naver.com>
2024-10-07 01:54:37 +0900

    dev environment setting

---
+++ week03/styled_component_practice/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+
+import './App.css'
+import CustomButton from "./components/custom-button.jsx";
+
+function App() {
+    return (
+        <>
+            <CustomButton/>
+        </>
+    )
+}
+
+export default App
