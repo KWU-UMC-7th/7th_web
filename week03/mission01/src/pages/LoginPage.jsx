caa61fac177b6e1cbfe03cb233fd40116cadf316
Eunjin Oh <ibsuny3214@naver.com>
2024-10-09 23:59:09 +0900

    mission01 success

---
+++ week03/mission01/src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const LoginPage = () => {
+  return (
+    <Wrapper>
+      <h1 style={{ color: "white" }}> Login Page</h1>{" "}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: #000000;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default LoginPage;
---
+++ week03/mission01/src/pages/SearchPage.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const SearchPage = () => {
+  return (
+    <Wrapper>
+      <h1 style={{ color: "white" }}>SearchPage</h1>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: #000000;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default SearchPage;
