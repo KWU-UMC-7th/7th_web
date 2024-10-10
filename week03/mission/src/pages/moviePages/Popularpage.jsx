67772614c667599487315a545ebe469b8418475c
dohyung <154952683+dohyung001@users.noreply.github.com>
2024-10-11 04:00:45 +0900

    weak3_제출

--- week03/mission/src/pages/moviePages/Popularpage.jsx
+++ week03/mission/src/pages/moviePages/Popularpage.jsx
@@ -1,12 +1,6 @@
 import styled from 'styled-components'
 import MoviePage from '../../components/Moviepage'
-const Conatiner = styled.div`
-width:100vw;
-height:100vh;
-background-color:black;
-color:white;
-padding:20px;
-`
+
 
 const PopularPage = () => {
   return (
